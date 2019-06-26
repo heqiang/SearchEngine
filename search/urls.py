114408fe7d95acf441163daf2bd7464405e25af1
hq <1422127065@qq.com>
2019-06-26 16:42:51 +0800

    解决了翻页一直是java的问题，剩下页码对应问题

--- search/urls.py
+++ search/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$', views.indexArticle),
-    url(r'^index',views.indexArticle),
-    url(r'^suggest/$',views.suggest_article,name="suggest"),
-    url(r'^search/$', views.Search_View, name="search"),
+    url(r'^$', views.IndexView),
+    url(r'^index',views.IndexView),
+    url(r'^suggest/$',views.SearchSuggest,name="suggest"),
+    url(r'^search/$', views.SearchView, name="search"),
 
 ]
--- search/views.py
+++ search/views.py
@@ -10,10 +10,10 @@ client=Elasticsearch(hosts=["127.0.0.1"])
 
 redis_cli=redis.StrictRedis()
 
-def  indexArticle(request):
+def  IndexView(request):
     return render(request,'index.html')
 
-def suggest_article(request):                                      # 搜索自动补全逻辑处理
+def SearchSuggest(request):                                      # 搜索自动补全逻辑处理
     key_words = request.GET.get('s', '')                        # 获取到请求词
     re_datas = []
     if key_words:
@@ -30,14 +30,18 @@ def suggest_article(request):                                      # 搜索自�
             re_datas.append(source["title"])
     return HttpResponse(json.dumps(re_datas), content_type="application/json")
 
-def Search_View(request):
-        key_word=request.GET.get('q',"")
-        page=request.GET.get('p','1')#页码的获取
+def SearchView(request):
+        key_words=request.GET.get('q',"")
+        print(key_words)
+        page = request.GET.get("p", "1")
+        print("当前页码是：")
+        print(page)
         try:
-            page=int(page)
+            page = int(page)
         except:
-            page=1
-        jobbole_count=redis_cli.get("jobbole_count")
+            page = 1
+
+        jobbole_count=redis_cli.get("jobbole_count")#搜索记录次数
         jobbole_count=int(jobbole_count)
 
         start_time=datetime.now()
@@ -46,7 +50,7 @@ def Search_View(request):
             body={
                 "query": {
                   "multi_match": {
-                    "query": key_word,
+                    "query": key_words,
                     "fields": ["title","tags","content"]
                   }
               },
@@ -61,13 +65,12 @@ def Search_View(request):
                         "content": {}  # 高亮字段
                     }
                 }
-
             }
-
         )
         end_time=datetime.now()
         last_seconds=(end_time-start_time).total_seconds()
         total_nums = response["hits"]["total"]
+        print()
         if total_nums>10:
             page_nums=int(total_nums/10)+1
         else:
@@ -91,12 +94,122 @@ def Search_View(request):
                   hit_dict["score"] = hit["_score"]
                   # 将内容加入到list
                   hit_list.append(hit_dict)
-        return render(request,"result.html",{"all_list":hit_list,
-                                             "key_word":key_word,
-                                             "total":total_nums,
-                                             "page":page,
-                                             "page_nums":page_nums,
-                                             "total_time":last_seconds,
-                                              "jobbole_count":jobbole_count,
-                                             # "top_search":top_search,
-                                             })
+        return render(request, "result.html", {"all_list":hit_list,
+                                                 "key_words":key_words,
+                                                 "total_nums":total_nums,
+                                                 "page":page,
+                                                 "page_nums":page_nums,
+                                                 "last_seconds":last_seconds,
+                                                  "jobbole_count":jobbole_count,
+                                                    # "top_search":top_search,
+                                                })
+# import json
+# from django.shortcuts import render
+# from django.views.generic.base import View
+# from search.models import ArticType
+# from django.http import HttpResponse
+# from elasticsearch import Elasticsearch
+# from datetime import datetime
+# import redis
+#
+# client = Elasticsearch(hosts=["127.0.0.1"])
+# redis_cli = redis.StrictRedis()
+#
+#
+# def IndexView(request):
+#     #首页
+#         topn_search = redis_cli.zrevrangebyscore("search_keywords_set", "+inf", "-inf", start=0, num=5)
+#         return render(request, "index.html", {"topn_search":topn_search})
+#
+# # Create your views here.
+# def SearchSuggest(request):
+#         key_words = request.GET.get('s','')
+#         re_datas = []
+#         if key_words:
+#             s = ArticType.search()
+#             s = s.suggest('my_suggest', key_words, completion={
+#                 "field":"suggest", "fuzzy":{
+#                     "fuzziness":2
+#                 },
+#                 "size": 10
+#             })
+#             suggestions = s.execute_suggest()
+#             for match in suggestions.my_suggest[0].options:
+#                 source = match._source
+#                 re_datas.append(source["title"])
+#         return HttpResponse(json.dumps(re_datas), content_type="application/json")
+#
+#
+# def SearchView(request):
+#
+#         key_words = request.GET.get("q","")
+#         s_type = request.GET.get("s_type", "article")
+#
+#         # redis_cli.zincrby("search_keywords_set", key_words)
+#
+#         # topn_search = redis_cli.zrevrangebyscore("search_keywords_set", "+inf", "-inf", start=0, num=5)
+#         page = request.GET.get("p", "1")
+#         try:
+#             page = int(page)
+#         except:
+#             page = 1
+#
+#         jobbole_count = redis_cli.get("jobbole_count")
+#         start_time = datetime.now()
+#         response = client.search(
+#             index= "jobbole",
+#             body={
+#                 "query":{
+#                     "multi_match":{
+#                         "query":key_words,
+#                         "fields":["tags", "title", "content"]
+#                     }
+#                 },
+#                 "from":(page-1)*10,
+#                 "size":10,
+#                 "highlight": {
+#                     "pre_tags": ['<span class="keyWord">'],
+#                     "post_tags": ['</span>'],
+#                     "fields": {
+#                         "title": {},
+#                         "content": {},
+#                     }
+#                 }
+#             }
+#         )
+#
+#         end_time = datetime.now()
+#         last_seconds = (end_time-start_time).total_seconds()
+#         total_nums = response["hits"]["total"]
+#         if (page%10) > 0:
+#             page_nums = int(total_nums/10) +1
+#         else:
+#             page_nums = int(total_nums/10)
+#         hit_list = []
+#         for hit in response["hits"]["hits"]:
+#             hit_dict = {}
+#             if "title" in hit["highlight"]:
+#                 hit_dict["title"] = "".join(hit["highlight"]["title"])
+#             else:
+#                 hit_dict["title"] = hit["_source"]["title"]
+#             if "content" in hit["highlight"]:
+#                 hit_dict["content"] = "".join(hit["highlight"]["content"])[:500]
+#             else:
+#                 hit_dict["content"] = hit["_source"]["content"][:500]
+#
+#             hit_dict["create_date"] = hit["_source"]["create_date"]
+#             hit_dict["url"] = hit["_source"]["link_url"]
+#             hit_dict["score"] = hit["_score"]
+#
+#             hit_list.append(hit_dict)
+#
+#         return render(request, "result.html", {"page":page,
+#                                                "all_hits":hit_list,
+#                                                "key_words":key_words,
+#                                                "total_nums":total_nums,
+#                                                "page_nums":page_nums,
+#                                                "last_seconds":last_seconds,
+#                                                "jobbole_count":jobbole_count,
+#                                                # "topn_search":topn_search
+#                                                })
+# #
